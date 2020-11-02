b8639cad1c3eea39bd4718f28dfe028472ac42e3
Пользователь <you@example.com>
2020-11-02 14:34:24 +0400

    Add project

---
+++ get/main.go
@@ -0,0 +1,75 @@
+package get
+
+import (
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/html/charset"
+)
+
+type setSite struct {
+	Proto       string
+	Name        string
+	Dom         string
+	Url         string
+	ServerCode  int
+	Server      []string
+	ServerPower []string
+	Body        string
+}
+
+func New(protokol, name, dom string) setSite {
+
+	s := setSite{}
+	s.Proto = protokol
+	s.Name = name
+	s.Dom = dom
+
+	// формеруем путь
+	s.Url = s.Proto + "://" + s.Name + "." + s.Dom
+
+	return s
+
+}
+
+// запрос на сервер
+func (s *setSite) Test() error {
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	r, err := http.NewRequest("GET", s.Url, nil)
+	if err != nil {
+		return err
+	}
+
+	r.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36")
+
+	resp, err := client.Do(r)
+	if err != nil {
+		return err
+	}
+
+	s.Server = resp.Header["Server"]
+	s.ServerPower = resp.Header["X-Powered-By"]
+	s.ServerCode = resp.StatusCode
+
+	//перекодируем
+	utf8, err := charset.NewReader(resp.Body, resp.Header.Get("Content-Type"))
+	if err != nil {
+		return err
+	}
+
+	if h, err := ioutil.ReadAll(utf8); err != nil {
+		s.Body = "error read"
+	} else {
+		s.Body = string(h)
+	}
+
+	//fmt.Println(s.Url, " [", resp.StatusCode, "] ---", resp.Header["X-Powered-By"], "----------------------------", resp.Header["Server"])
+
+	return nil
+
+}
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"./comb"
+	"./get"
+	"fmt"
+)
+
+const (
+	GOR  = 500 // количество одновреммено работающий потоков
+	PROT = "https" // протокол
+	DOM  = "ua" // доменная зона
+)
+
+var can chan int = make(chan int, GOR)
+
+func testSite(name string, c chan int, i int) {
+
+	// освобождаем место в канале
+	defer func() {
+		<-c
+	}()
+
+	// опрашиваем url
+	s := get.New(PROT, name, DOM)
+	err := s.Test()
+	if err == nil{
+
+	}
+	fmt.Println(i, ". ", s.Url, "  -", s.ServerCode, "   [", err, "]")
+
+}
+
+func main() {
+
+	//начальная строка
+	a := "0"
+	// просто счетчик
+	i := 1
+
+	for {
+
+		// если есть возможность создать поток
+		if len(can) < GOR {
+			// занимаем место в канале
+			can <- 1
+
+			// создаем гарутину опроса по url
+			go testSite(a, can, i)
+
+			// создаем следующую комбинацию
+			ceb, err := comb.Next(a)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			// сохраняем комбинацию
+			a = ceb
+			// увеличиваем счетчик
+			i++
+
+		}
+
+	}
+
+}
